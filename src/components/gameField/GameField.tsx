b2833aed4447a3ef462cec888aa4a388d0b41909
Ture Thörnvik <tthornvik@deloittedigital.se>
2022-04-01 17:03:33 +0200

    gamefield

---
+++ src/components/gameField/GameField.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './GameField.scss'
+import PlayingField from '../playingField/PlayingField'
+
+const GameField = () => (
+  <div className='gamefield'>
+    <PlayingField />
+  </div>
+)
+
+export default GameField
