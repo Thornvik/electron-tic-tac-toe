baac94e0a4baa1cecd1352a2d14324d148673552
Ture Thörnvik <tthornvik@deloittedigital.se>
2022-04-01 11:28:17 +0200

    adding sass support

--- src/renderer.ts
+++ src/renderer.ts
@@ -26,7 +26,7 @@
  * ```
  */
 
-import './index.css'
+import './global.scss'
 import './app'
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack')
--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,17 +1,22 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const rules = require('./webpack.rules');
 const plugins = require('./webpack.plugins');
 
 rules.push({
-  test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+  test: /\.s[ac]ss$/i,
+  use: [
+    'style-loader',
+    'css-loader',
+    'sass-loader'
+  ]
 });
 
 module.exports = {
   module: {
-    rules,
+    rules
   },
   plugins: plugins,
   resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
-  },
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css']
+  }
 };
