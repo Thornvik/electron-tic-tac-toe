5c6957dfc106435885336104bc7d06ea648dfd56
Ture Thörnvik <tthornvik@deloittedigital.se>
2022-04-04 15:16:47 +0200

    feat: playing field winning logic

--- src/components/playingField/PlayingField.tsx
+++ src/components/playingField/PlayingField.tsx
@@ -1,14 +1,24 @@
+import React, { useContext, useEffect } from "react"
+import { GameContext } from "../../context/GameContext"
+import { checkWin } from "../../utils/CheckWin"
 import Tile from "../tile/Tile"
-import React from "react"
 import './PlayingField.scss'
 
 const SIZE = 9
 
 const PlayingField = () => {
+  const { playingField, players } = useContext(GameContext)
+  const winner = checkWin(playingField, players)
+
+  useEffect(() => {
+    if (!winner) return
+    alert(winner + ' wins')
+  }, [winner])
+
   return (
     <div className="playingfield">
       {[...Array(SIZE)].map((_, i) => (
-        <Tile light={i%2 === 0}/>
+        <Tile id={i} key={i} light={i%2 === 0}/>
       ))}
     </div>
   )
