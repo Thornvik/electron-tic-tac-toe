96f11911b6393253f53c21e8ace93a79f2818ac5
Ture Thörnvik <tthornvik@deloittedigital.se>
2022-04-04 10:57:04 +0200

    feat: using game provider

--- src/components/gameField/GameField.tsx
+++ src/components/gameField/GameField.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './GameField.scss'
 import PlayingField from '../playingField/PlayingField'
+import { GameProvider } from '../../context/GameContext'
 
 const GameField = () => (
-  <div className='gamefield'>
-    <PlayingField />
-  </div>
+  <GameProvider>
+    <div className='gamefield'>
+      <PlayingField />
+    </div>
+  </GameProvider>
 )
 
 export default GameField
