6ef51a27e48a576047a0bd84327aa7672ac60746
Ture Thörnvik <tthornvik@deloittedigital.se>
2022-04-04 10:58:05 +0200

    feat: tile now sets the playing field aswell

--- src/components/tile/Tile.tsx
+++ src/components/tile/Tile.tsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { GameContext, Turn } from '../../context/GameContext'
 import useTileClick from '../../hooks/useTileClick'
 import './Tile.scss'
 
 interface TileProps {
-  light: boolean
+  light: boolean,
+  id: number
 }
 
 const Tile = ( props: TileProps) => {
-  const { light } = props
+  const { light, id } = props
   const { clicked, clickHandler } = useTileClick()
+  const { turn, setTurn, playingField } = useContext(GameContext)
 
   const playerColor = light ? ' tile_clicked--blue' : ' tile_clicked--red'
   const classes = !clicked ? 'tile' : 'tile' + playerColor
+
+  useEffect(() => {
+    if (clicked) {
+      playingField[id] = turn
+      if (turn === Turn.p1) return setTurn(Turn.p2)
+      return setTurn(Turn.p1)
+    }
+  }, [clicked])
 
   return (
     <div
