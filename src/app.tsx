3c4b7887420c9491aa0588daa88cefa55af91059
Ture Thörnvik <tthornvik@deloittedigital.se>
2022-04-04 15:17:02 +0200

    feat: using gamefield component

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import * as ReactDOMClient from 'react-dom/client'
+import GameField from './components/gameField/GameField'
 
 const container = document.getElementById('app')
 
 const root = ReactDOMClient.createRoot(container)
 
-root.render(<h1>hello world</h1>)
+root.render(<GameField />)
